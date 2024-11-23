638d886a852e90ee6f09ae6222efcf4770e6f674
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-11-23 22:42:57 +0500

    upgrade tailwind

--- components/common/html-map.tsx
+++ components/common/html-map.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+"use client";
+
+// import React from "react";
+
+// export default function HtmlMap() {
+//   return (
+//     <iframe
+//       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443088.05185929226!2d-95.73096574031447!3d29.817364672769074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX%2C%20USA!5e0!3m2!1sen!2s!4v1713121772381!5m2!1sen!2s"
+//       width="100%"
+//       height="400"
+//       className="border-none"
+//       allowFullScreen={true}
+//       loading="lazy"
+//       referrerPolicy="no-referrer-when-downgrade"
+//     ></iframe>
+//   );
+// }
+
+import * as React from "react";
+import { Map } from "@vis.gl/react-maplibre";
+import "maplibre-gl/dist/maplibre-gl.css"; // See notes below
 
 export default function HtmlMap() {
   return (
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443088.05185929226!2d-95.73096574031447!3d29.817364672769074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX%2C%20USA!5e0!3m2!1sen!2s!4v1713121772381!5m2!1sen!2s"
-      width="100%"
-      height="400"
-      className="border-none"
-      allowFullScreen={true}
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-    ></iframe>
+    <Map
+      initialViewState={{
+        longitude: -100,
+        latitude: 40,
+        zoom: 3.5,
+      }}
+      style={{ width: "100%", height: 400 }}
+      mapStyle="https://demotiles.maplibre.org/style.json"
+    />
   );
 }
