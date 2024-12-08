832f7e828d4ef9b13ebe16e5631227229b8672da
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-12-08 18:03:26 +0500

    update

--- components/common/html-map.tsx
+++ components/common/html-map.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { Map } from "@vis.gl/react-maplibre";
-import "maplibre-gl/dist/maplibre-gl.css"; // See notes below
+import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function HtmlMap() {
   return (
