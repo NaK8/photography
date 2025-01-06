5ec525a9fec17799489bcc3627a2b58ed022c7b7
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2025-01-06 20:19:08 +0500

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-    optimizePackageImports: ["lucide-react", "motion", "react-multi-carousel"],
+    optimizePackageImports: ["lucide-react", "react-multi-carousel"],
     ppr: "incremental",
   },
 };
