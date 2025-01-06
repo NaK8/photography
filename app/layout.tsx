3f42e8934cb8e0aff261a93e0e2b0e678c4185f0
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2025-01-06 18:54:54 +0500

    optimiize font

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,12 @@ import Footer from "@/components/common/footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
+  display: "swap",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
