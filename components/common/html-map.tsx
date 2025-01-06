9da1d6423eaae535df71d7a7355de5f81ff3753c
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2025-01-06 19:47:43 +0500

    update map

--- components/common/html-map.tsx
+++ components/common/html-map.tsx
@@ -1,19 +1,14 @@
-"use client";
-
 import * as React from "react";
-import { Map } from "@vis.gl/react-maplibre";
-import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function HtmlMap() {
   return (
-    <Map
-      initialViewState={{
-        longitude: -100,
-        latitude: 40,
-        zoom: 3.5,
-      }}
-      style={{ width: "100%", height: 400 }}
-      mapStyle="https://demotiles.maplibre.org/style.json"
-    />
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14044788.945745127!2d-110.72259188288965!3d30.749210588716156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864070360b823249%3A0x16eb1c8f1808de3c!2sTexas%2C%20USA!5e0!3m2!1sen!2s!4v1736173681757!5m2!1sen!2s"
+      width="100%"
+      height="400"
+      allowFullScreen={true}
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
-    optimizePackageImports: ["lucide-react", "motion"],
+    optimizePackageImports: ["lucide-react", "motion", "react-multi-carousel"],
     ppr: "incremental",
   },
 };
