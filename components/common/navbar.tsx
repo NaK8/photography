7801c6d56065d06d32134c5e59a450f4c104c16e
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2025-04-04 01:04:47 +0500

    update deps and prefetch nav links

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -6,19 +6,29 @@ export default function Navbar() {
     <nav>
       <ul className="flex gap-4">
         <li>
-          <Link href={"/"}>Home</Link>
+          <Link prefetch href={"/"}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href={"/about"}>About</Link>
+          <Link prefetch href={"/about"}>
+            About
+          </Link>
         </li>
         <li>
-          <Link href={"/blog"}>Blog</Link>
+          <Link prefetch href={"/blog"}>
+            Blog
+          </Link>
         </li>
         <li>
-          <Link href={"/gallery"}>Gallery</Link>
+          <Link prefetch href={"/gallery"}>
+            Gallery
+          </Link>
         </li>
         <li>
-          <Link href={"/contact"}>Contact</Link>
+          <Link prefetch href={"/contact"}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
